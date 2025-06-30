d5c535b2a3c06aa14a5c069b9147393fcf33f90f
Lysyonok Gregory <bloodamerika@gmail.com>
2025-06-30 13:11:37 +0300

    server.js updated

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.post('/api/equipment', async (req, res) => {
     return res.status(400).json({ message: 'Не все поля переданы' });
   }
   try {
-    const check = await client.query(`SELECT sn = $1 FROM equipment WHERE status = 'В ремонте'`, [sn]);
+    const check = await client.query(`SELECT sn = $1 FROM equipment WHERE status = 'В ремонте' LIMIT 1`, [sn]);
     if(check.rowCount){
         return res
         .status(409)
